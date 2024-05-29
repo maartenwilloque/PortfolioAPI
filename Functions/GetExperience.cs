3755a288d3b3de04fae278a135db15355e23ab1b
maartenwilloque <maarten.willoque@qubix.be>
2024-05-29 21:59:51 +0200

    cors added

--- Functions/GetExperience.cs
+++ Functions/GetExperience.cs
@@ -3,8 +3,6 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
